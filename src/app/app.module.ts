8f38e8db48e1bb74a5b06a0c7242b4acfbc79d2c
srikanth <budde.sree@gmail.com>
2022-01-21 17:47:31 +0530

    meassge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {MatSliderModule} from '@angular/material/slider';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatBottomSheetModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
